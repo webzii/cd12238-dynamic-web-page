e70ec2fbe53f00de6ab52b928fe8aac13e8e808c
Takweed Git and GitHub Course <tpagitngithub@gmail.com>
2024-07-07 19:40:57 +0300

    Built the Nav Bar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-/** TODO: Build the navigation bar using JavaScript **/
-
-/** TODO: Add smooth scrolling **/
-
-/** TODO: Add an active state **/
-
-/** TODO: Add a comment form **/
+var section1 = document.getElementById('section1');
+var section2 = document.getElementById('section2');
+var section3 = document.getElementById('section3');
+var section4 = document.getElementById('section4');
+var section5 = document.getElementById('section5');
+var sections = [section1, section2, section3, section4, section5];
+console.log(sections);
