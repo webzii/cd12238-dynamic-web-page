e36d1a246747246ec938867a0c7607066a4b1b4c
Takweed Git and GitHub Course <tpagitngithub@gmail.com>
2024-07-09 10:39:16 +0300

    Added Comments Section

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var section2 = document.getElementById('section2');
 var section3 = document.getElementById('section3');
 var section4 = document.getElementById('section4');
 var section5 = document.getElementById('section5');
+var section6 = document.getElementById('commentsSection')
 var sections = [section1, section2, section3, section4, section5];
 console.log(sections);
 var section1NavBarLink = document.getElementById('l1');
@@ -10,6 +11,7 @@ var section2NavBarLink = document.getElementById('l2');
 var section3NavBarLink = document.getElementById('l3');
 var section4NavBarLink = document.getElementById('l4');
 var section5NavBarLink = document.getElementById('l5');
+var section6NavBarLink = document.getElementById('l6')
 section1NavBarLink.addEventListener('click', function(event) {
     event.preventDefault();
     section1.scrollIntoView(
@@ -45,6 +47,14 @@ section4NavBarLink.addEventListener('click', function(event) {
 section5NavBarLink.addEventListener('click', function(event) {
     event.preventDefault();
     section5.scrollIntoView(
+        {
+            behavior: 'smooth'
+        }
+    )
+})
+section6NavBarLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    section6.scrollIntoView(
         {
             behavior: 'smooth'
         }
